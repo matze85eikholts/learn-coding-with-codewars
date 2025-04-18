c27bc8c9f1b257d6409941fd1e751aa43b54a392
Matvey Eikholts <matze85eikholts@gmail.com>
2025-04-18 13:49:27 +0600

    java 8 kyu easy logs

---
+++ java/8 kyu/01 easylogs/EasyLogs.java
@@ -0,0 +1,10 @@
+public class EasyLogs {
+
+  public static double logs(double x, double a, double b) {
+    
+        double product = a * b;
+        return Math.log(product) / Math.log(x);
+
+  }
+
+}
